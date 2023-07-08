bc13b32968de1f5d9dde80f30af8585b6104dc91
matchild <mt.child@gmx.com>
2023-07-08 12:53:02 +0200

    Polished interface

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
-import numpy as np
+from time import sleep
 
 def main():
     #Initialize openAI key
@@ -16,9 +16,11 @@ def main():
     qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=db.as_retriever())
 
     while True:
-        query=input('Enter your query and press enter. Press Ctrl+C to exit.\n')
+        query=input('Enter your query and press enter. Press Ctrl+C to exit.\n\n')
         response=qa.run(query)
         print(response)
+        print('\n')
+        sleep.(1)
 
 if __name__ == '__main__':
     main()
